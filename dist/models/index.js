7e8b00a50c79b3359c95703de7ada96aa221d2ec
Divyansh Patel <pateldivyansh131@gmail.com>
2025-08-21 11:26:34 +0530

    Remove dist folder from repo

--- dist/models/index.js
+++
@@ -1,6 +0,0 @@
-export { default as User } from './User.js';
-export { default as Evidence } from './Evidence.js';
-export { default as AccessRequest } from './AccessRequest.js';
-export { default as SignupRequest } from './SignupRequest.js';
-export { default as WalletPermission } from './WalletPermission.js';
-export { default as BlockchainRecord } from './BlockchainRecord.js';
